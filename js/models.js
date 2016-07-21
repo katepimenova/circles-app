a493ceb44a7f3ce2ad6ed3958a50d7f883934468
Kate Pimenova <kpimenova@mirantis.com>
2016-07-21 15:40:56 +0300

    Backbone model validation added

--- js/models.js
+++ js/models.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import Backbone from 'backbone';
 import LocalStorage from '../lib/backbone.localStorage';
 
@@ -5,11 +6,27 @@ var models = {};
 
 models.Circle = Backbone.Model.extend({
   constructorName: 'Circle',
-  defaults: {
-    id: null,
-    x: null, // x coordinates
-    y: null, // y coordinates
-    z: null // radius coordinates
+  validate({x, y, r}, {radiusMax, xMax, yMax}) {
+    var errors = {};
+    if (Number(r) <= 0) {
+      errors.r = 'Invalid radius';
+    }
+    if (Number(x) < 0) {
+      errors.x = 'Invalid X coordinate';
+    }
+    if (Number(y) < 0) {
+      errors.y = 'Invalid Y coordinate';
+    }
+    if (!errors.x && Number(x) > xMax) {
+      errors.x = 'The new circle won\'t fit the viewport';
+    }
+    if (!errors.y && Number(y) > yMax) {
+      errors.y = 'The new circle won\'t fit the viewport';
+    }
+    if (!errors.r && Number(r) * 2 > radiusMax) {
+      errors.r = 'Sum of circles diameters cannot be larger than the viewport width (must be <= 100)';
+    }
+    return _.isEmpty(errors) ? null : errors;
   }
 });
 
